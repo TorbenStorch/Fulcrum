594c403946f4fd4417b9a9947ca32ff0221ebbb6
Philipp Petry <pp.petry@outlook.de>
2022-07-05 20:36:50 +0200

    Implementation of Time Scrolling

---
+++ Assets/Scripts/Philipp/Time Mechanic/TearTimeMeasurement.cs
@@ -0,0 +1,75 @@
+/*-------------------------------------------------------
+Creator: Philipp Petry
+Project: Fulcrum
+Last change: 01-07-2022
+Topic: Define winning condition for the attached Tear Object
+---------------------------------------------------------*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TearInteractable))]
+public class TearTimeMeasurement : MonoBehaviour
+{
+    [SerializeField] private bool pastWins;
+
+    private Material _material;
+    private TearInteractable _tearIneractableScript;
+    private float _pastValue, _futureValue;
+
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float minPastValue, maxPastValue, minFutureValue, maxFutureValue;
+
+    [HideInInspector]
+    public bool tearCorrect = false;
+
+    void Awake()
+    {
+        _tearIneractableScript = GetComponent<TearInteractable>();
+        GetMaterial();
+    }
+
+    void Update()
+    {
+        GetMaterial();
+        if (_tearIneractableScript.isGrabbed)
+        {
+            if (pastWins)
+            {
+                if (_pastValue > minPastValue && _pastValue < maxPastValue)
+                    tearCorrect = true;
+                
+                else 
+                    tearCorrect = false;
+            } else if (!pastWins)
+            {
+                if (_futureValue > minFutureValue && _futureValue < maxFutureValue)
+                
+                    tearCorrect = true;  
+
+                else
+                    tearCorrect = false;
+            }
+        }
+
+        if (tearCorrect)
+            Debug.Log(gameObject.name + " in win range");
+        else
+            Debug.Log(gameObject.name + " not in win range anymore");
+    }
+
+
+
+    private void GetMaterial()
+    {
+        _material = gameObject.GetComponent<SkinnedMeshRenderer>().sharedMaterial;
+
+        if (_material != null)
+        {
+            _pastValue = _material.GetFloat("_BlendToPast");
+            _futureValue = _material.GetFloat("_BlendToFuture");
+        }
+        else
+            Debug.Log("No Material has been found for Game Object: " + gameObject.name.ToString());
+    }
+}
---
+++ Assets/Scripts/Philipp/Time Mechanic/TimeScrolling.cs
@@ -0,0 +1,52 @@
+/*-------------------------------------------------------
+Creator: Philipp Petry
+Project: Fulcrum
+Last change: 01-07-2022
+Topic: Adjusts Blendshape Value according to the controller rotation
+---------------------------------------------------------*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScrolling : MonoBehaviour
+{
+    public SkinnedMeshRenderer _tearRenderer;
+    public float _blendValue;
+    public float _controllerRotZ;
+    public Raycast _rayCastScript;
+
+    private GameObject controllerRight;
+
+    void Start()
+    {
+        controllerRight = this.gameObject;
+    }
+
+    void Update()
+    {
+        _controllerRotZ = controllerRight.transform.rotation.z * 100;
+        _blendValue = _controllerRotZ;
+        
+
+        if (_rayCastScript._rayCastHit == true && _rayCastScript.isGrabbed == true)
+        {
+            _tearRenderer = _rayCastScript.targetRenderer;
+
+            if (_blendValue >= 0)
+            {
+                _tearRenderer.SetBlendShapeWeight(1, _blendValue);
+                _rayCastScript.targetMaterial.SetFloat("_BlendToPast", _blendValue/100);
+            }
+            else if (_blendValue < 0)
+            {
+                _tearRenderer.SetBlendShapeWeight(0, _blendValue - 2 * _blendValue);
+                _rayCastScript.targetMaterial.SetFloat("_BlendToFuture", (_blendValue - 2 * _blendValue) / 100);
+            }
+        }
+
+        // Debug.Log("Blend to Past Value: " + _rayCastScript.targetMaterial.GetFloat("_BlendToPast").ToString());
+        // Debug.Log("Blend to Past Future: " + _rayCastScript.targetMaterial.GetFloat("_BlendToFuture").ToString());
+        // Debug.Log("controller rotation z = " + _blendValue / 100);
+    }
+}
